72510fad98731a1a989e3509ee01c6552d213e73
akihide-hatano <a.h.19841202@gmail.com>
2025-06-25 09:48:22 +0900

    Models/Medication.phpを修正しました。

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Medication extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'medication_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable =[
+        'medication_name',
+        'dosage',
+        'notes',
+        'effect',
+        'side_effects'
+    ];
+
+    public function timingTags(){
+        return $this->belongsToMany(
+        TimingTag::class,
+        'medication_timing_tags',
+        'medication_id',
+        'timing_tag_id'
+        );
+    }
 }
