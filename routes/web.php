382b300e62e6b3d20f1fdff712557e059a719389
akihide-hatano <a.h.19841202@gmail.com>
2025-06-25 17:10:39 +0900

    web.phpのルーティングを追加し、コントローラーを作成しました。

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MedicationController;
+
+//トップページ
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+//薬に関するルーティング
+Route::get('/medications',[MedicationController::class,'index'])->name('medications.index');
+Route::get('/medications/create',[MedicationController::class,'create'])->name('medications.create');
+Route::post('/medications',[MedicationController::class,'store'])->name('medications.store');
+Route::get('/medications/{medication}', [MedicationController::class, 'show'])->name('medications.show');
+Route::get('/medications/{medication}/edit', [MedicationController::class, 'edit'])->name('medications.edit');
+Route::put('/medications/{medication}', [MedicationController::class, 'update'])->name('medications.update');
+Route::patch('/medications/{medication}', [MedicationController::class, 'update']); // PUTと同じメソッドを使うことが多い
+Route::delete('/medications/{medication}', [MedicationController::class, 'destroy'])->name('medications.destroy');
 
 Route::get('/', function () {
     return view('welcome');
