9872519795eab733a28ca94ad086fd368bedd1bb
akihide-hatano <a.h.19841202@gmail.com>
2025-06-25 15:19:12 +0900

    MedicationTimingTagを修正しました。

--- app/Models/MedicationTimingTag.php
+++ app/Models/MedicationTimingTag.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MedicationTimingTag extends Model
 {
-    //
+    use HasFactory;
+
+    public $table = 'medication_timing_tags';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+    'medication_id',
+    'timing_tag_id',
+    ];
+
+    public function medication(){
+        return $this->belongsTo(Medication::class,'medication_id','medication_id');
+    }
+
+    public function timingTag(){
+        return $this->belongsTo(TimingTag::class, 'timing_tag_id', 'timing_tag_id');
+    }
 }
