ecf501e99ad49bd62eec10d2da574da2a2294ff8
akihide-hatano <a.h.19841202@gmail.com>
2025-06-25 15:05:30 +0900

    Models/PostTimingを作成しました。

--- app/Models/PostTiming.php
+++ app/Models/PostTiming.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PostTiming extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'posts_timing';
+
+    public $incrementing = false;
+
+    // マスアサインメント可能なカラム
+    protected $fillable = [
+        'post_id',
+        'timing_tag_id',
+        'is_completed',
+    ];
+
+    public $timestamps = true;
+
+    public function post(){
+        return $this->belongsTo(Post::class, 'post_id', 'post_id');
+    }
+
+    public function timingTag(){
+        return $this->belongsTo(TimingTag::class, 'timing_tag_id', 'timing_tag_id');
+    }
 }
