0d49043775ebeb8068ba0cc8db111b4354866ec0
akihide-hatano <a.h.19841202@gmail.com>
2025-07-08 13:14:01 +0900

    dashboard.blade.php: Update dashboard layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,7 +26,8 @@
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard-check mr-2"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><path d="m9 14 2 2 4-4"/></svg>
                             今日の記録へ
                         </a>
-                        <div class="mt-4">
+                        <div class="mt-4 flex items-center justify-center gap-3">
+                            <img class="w-6 h-6" src="{{asset('images/calendar.png')}}" alt="カレンダーのアイコン">
                             <a href="{{ route('posts.calendar') }}" class="text-blue-600 hover:underline text-lg">
                                 カレンダー全体を見る
                             </a>
